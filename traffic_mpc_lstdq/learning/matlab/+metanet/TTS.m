ba70aa557eec5590c657d08f4ae68ce03671ec42
Filippo Airaldi <f.airaldi@tudelft.nl>
2022-03-19 22:22:29 +0100

    made metanet costs into casadi functions

--- traffic_mpc_lstdq/learning/matlab/+metanet/TTS.m
+++ traffic_mpc_lstdq/learning/matlab/+metanet/TTS.m
@@ -1,3 +1,11 @@
-function J = TTS(w, rho, T, L, lanes)
-    J = T * (sum(w, 1) + sum(rho * L * lanes, 1));
+function J = TTS(n_origins, n_links, T, L, lanes)
+
+    w = casadi.SX.sym('w', n_origins, 1);
+    rho = casadi.SX.sym('rho', n_links, 1);
+
+    TTS = T * (sum(w, 1) + sum(rho * L * lanes, 1));
+    assert(isequal(size(TTS), [1, 1]));
+    
+    J = casadi.Function('tts_cost', {w, rho}, {TTS}, ...
+        {'w', 'rho'}, {'J_TTS'});
 end
--- traffic_mpc_lstdq/learning/matlab/+metanet/rate_variability.m
+++ traffic_mpc_lstdq/learning/matlab/+metanet/rate_variability.m
@@ -1,3 +1,11 @@
-function J = rate_variability(r_last, r)
-   J = sum(diff([r_last, r], 1, 2).^2, 2);
+function J = rate_variability(n_ramps, Nc)
+
+    r_last = casadi.SX.sym('r_last', n_ramps, 1);
+    r = casadi.SX.sym('r', n_ramps, Nc);
+
+   variability = sum(sum(diff([r_last, r], 1, 2).^2, 2), 1);
+   assert(isequal(size(variability), [1, 1]));
+
+    J = casadi.Function('rate_var_cost', {r_last, r}, {variability}, ...
+        {'r_last', 'r'}, {'J_rate_var'});
 end
