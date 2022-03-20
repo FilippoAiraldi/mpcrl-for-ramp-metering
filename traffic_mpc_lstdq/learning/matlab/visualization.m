4b065f744af65fd7066b43ed1f12ac2b08500006
Filippo Airaldi <f.airaldi@tudelft.nl>
2022-03-20 15:36:42 +0100

    improvements to visualization

--- traffic_mpc_lstdq/learning/matlab/visualization.m
+++ traffic_mpc_lstdq/learning/matlab/visualization.m
@@ -27,31 +27,35 @@ Table = {... % all entries must be strings
     'mean ep exec time', ...
         duration(0, 0, mean(cell2mat(exec_times)), 'Format', 'hh:mm:ss');
 
-    % types of costs
-    'RLMPC', delimiter; ...
+    % MPC details
+    'MPC', delimiter; ...
     'Np/Nc/M', sprintf('%i/%i/%i', Np, Nc, M); ...
     'type cost initial', Vcost.name_out(); ...
     'type cost stage', Lcost.name_out(); ...
     'type cost terminal', Tcost.name_out(); ...
     'max iter', solver_opts.max_iter; ... 
-    'rate var penalty', rate_var_penalty; ...
+    'rate var penalty weight', rate_var_penalty; ...
     'max queue', max_queue; ...
+
+    % RL details
+    'RL', delimiter; ...
     'discount', discount; ...
     'learning rate', lr; ...
+    'constraint violation penalty', con_violation_penalty; ...
 
-    % learning
+    % learning outcomes
     'LEARNING', delimiter; ...
-    'updates at (iter/time)', sprintf('%s / %s', ...
-        mat2str(rl_update_at), mat2str(rl_update_at / K * Tfin)); ...
+    'update frequency (iter)', rl_update_freq; ...
     'a (true)', sprintf('%7.3f', true_pars.a);...
     'v_free (true/initial/final)', sprintf('%7.3f / %7.3f / %7.3f', ...
         true_pars.v_free, rl_pars.v_free(1), rl_pars.v_free(end));...
     'rho_crit (true/initial/final)', sprintf('%7.3f / %7.3f / %7.3f', ...
         true_pars.rho_crit, rl_pars.rho_crit(1), rl_pars.rho_crit(end));...
     };
+warning('still need to add for each learnable param the initial and final value')
     
 Table = string(Table);
-width = max(arrayfun(@(x) strlength(x), Table(:, 1))) + 6;
+width = max(arrayfun(@(x) strlength(x), Table(:, 1))) + 4;
 s = '';
 for i = 1:size(Table, 1)
     if all(isstrprop(Table(i, 1),'upper'))
@@ -70,13 +74,15 @@ fprintf(s)
 
 
 %% plotting
+step = min(step, M);
+
 % create figure
 figure;
 tiledlayout(4, 4, 'Padding', 'none', 'TileSpacing', 'compact')
 sgtitle(runname, 'Interpreter', 'none')
 
 
-% plot traffic quantities
+% plot time-based quantities
 t_tot = (0:(episodes * K - 1)) * T;
 origins_tot = structfun(@(x) cell2mat(x), origins, 'UniformOutput', false);
 links_tot = structfun(@(x) cell2mat(x), links, 'UniformOutput', false);
@@ -97,29 +103,44 @@ plot(t_tot(1:step:end), links_tot.density(:, 1:step:end)', '-')
 hlegend(3) = legend('\rho_{L1}', '\rho_{L2}', '\rho_{L3}');
 ylabel('density (veh/km)')
 
-ax(4) = nexttile(9); 
+ax(4) = nexttile(6);
+slack_tot = mean(cell2mat(slack), 1);
+plot(linspace(t_tot(1), t_tot(end), length(slack_tot)), slack_tot);
+ylabel('slack \sigma')
+
+ax(5) = nexttile(9); 
 plot(t_tot(1:step:end), repmat(D(:, 1:step:end), 1, episodes)')
-hlegend(4) = legend('d_{O1}', 'd_{O2}');
+hlegend(5) = legend('d_{O1}', 'd_{O2}');
 ylabel('origin demand (veh/h)')
 % ax(5).YLim(2) = 4000;
 
-ax(5) = nexttile(10); hold on
+ax(6) = nexttile(10); hold on
 plot(t_tot(1:step:end), origins_tot.queue(:, 1:step:end)')
 plot([t_tot(1), t_tot(end)], [max_queue, max_queue], '-.k')
 hold off
-hlegend(5) = legend('\omega_{O1}', '\omega_{O2}', 'max \omega_{O2}');
+hlegend(6) = legend('\omega_{O1}', '\omega_{O2}', 'max \omega_{O2}');
 ylabel('queue length (veh)')
 
-ax(6) = nexttile(13); 
+ax(7) = nexttile(13); 
 plot(t_tot(1:step:end), origins_tot.flow(:, 1:step:end)')
-hlegend(6) = legend('q_{O1}', 'q_{O2}');
+hlegend(7) = legend('q_{O1}', 'q_{O2}');
 ylabel('origin flow (veh/h)')
 
-ax(7) = nexttile(14); hold on,
-ax(7).ColorOrderIndex = 2;
+ax(8) = nexttile(14); hold on,
+ax(8).ColorOrderIndex = 2;
 stairs(t_tot(1:step:end), origins_tot.rate(:, 1:step:end)')
-hlegend(7) = legend('r_{O2}');
+hlegend(8) = legend('r_{O2}');
 ylabel('metering rate')
+
+ax(9) = nexttile(7);
+L_tot = full(Lrl(origins_tot.queue, links_tot.density));
+plot(t_tot(1:step:end), L_tot(:, 1:step:end))
+ylabel('L')
+
+ax(10) = nexttile(8);
+td_error_tot = 1:240; % placeholder
+plot(linspace(1, episodes, length(td_error_tot)), td_error_tot)
+ylabel('TD error \tau')
 
 linkaxes(ax, 'x')
 for i = 1:length(ax)
@@ -129,34 +150,25 @@ for i = 1:length(ax)
 %     hold(ax(i), 'on')
 %     plot(ax(i), (1:episodes) * Tfin, [0, ax(i).YLim(2)], ':k', 'LineWidth', 0.25)
 %     hold(ax(i), 'off')
-    hlegend(i).String = hlegend(i).String(1:end-episodes+1);
+    if isa(hlegend(i), 'matlab.graphics.illustration.Legend')
+        hlegend(i).String = hlegend(i).String(1:end-episodes+1);
+    end
     ax(i).YLim(1) = 0;
 end
 
 
-% plot learning quantities
+% plot episode-based quantities
 ax = matlab.graphics.axis.Axes.empty;
 
 ax(1) = nexttile(3, [1, 2]);
-plot(1:episodes, cellfun(@(x) sum(x), objective))
-ylabel('objective')
+performance = arrayfun(@(ep) full(sum(Lrl(origins.queue{ep}, links.density{ep}))), 1:episodes);
+plot(1:episodes, performance)
+ylabel('J(\pi)')
 
-ax(2) = nexttile(7);
-slack_tot = mean(cell2mat(slack), 1);
-plot(linspace(1, episodes, length(slack_tot)), slack_tot)
-ylabel('slack')
-
-ax(3) = nexttile(8);
-td_error_tot = cell2mat(objective);
-plot(linspace(1, episodes, length(td_error_tot)), td_error_tot)
-ylabel('TD error')
+warning('still need to add plots for each learnable param ')
 
 % tiles 11, 12, 15, 16 for showing convergences of learned parameters
 
-% ax(3) = nexttile(11);
-% plot()
-% plot v_free and rho_crit parameters as they are updated. Plot horizontal
-% line with the true parameters
 
 linkaxes(ax, 'x')
 for i = 1:length(ax)
