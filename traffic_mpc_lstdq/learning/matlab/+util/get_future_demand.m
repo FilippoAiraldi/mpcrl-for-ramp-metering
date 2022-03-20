6fa0ddc90affc7e5292ebbf24a5c3ebbea1b101c
Filippo Airaldi <f.airaldi@tudelft.nl>
2022-03-20 23:09:53 +0100

    opted for padding the demand to avoid out-of-bound access

--- traffic_mpc_lstdq/learning/matlab/+util/get_future_demand.m
+++ traffic_mpc_lstdq/learning/matlab/+util/get_future_demand.m
@@ -1,12 +1,10 @@
-function d = get_future_demand(D, k, K, M, Np)
-    % d = GET_FUTURE_DEMAND(D, k, K, M, Np) From the total demands D, gets
-    %   the demand from k to k+M*Np+1. Pads the result if k is close 
-    %   to the length of D.
-
-    if k <= K - M * Np + 1
-        d = D(:, k:k + M * Np - 1);
-    else
-        d = [D(:, k:end), repmat(D(:, end), 1, M * Np - 1 - K + k)];
-    end
+function d = get_future_demand(D, ep, k, K, M, Np) 
+%     if k <= K - M * Np + 1
+%         d = D(:, k:k + M * Np - 1);
+%     else
+%         d = [D(:, k:end), repmat(D(:, end), 1, M * Np - 1 - K + k)];
+%     end
+    idx = k + (ep - 1) * K;
+    d = D(:, idx:idx + M * Np - 1);
 end
 
