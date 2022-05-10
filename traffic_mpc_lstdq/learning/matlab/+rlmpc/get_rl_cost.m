1325abe0a9c83ab4663ac14923570fc919f2716d
Filippo Airaldi <f.airaldi@tudelft.nl>
2022-05-10 20:49:52 +0200

    streamlined computation of rl stage cost

--- traffic_mpc_lstdq/learning/matlab/+rlmpc/get_rl_cost.m
+++ traffic_mpc_lstdq/learning/matlab/+rlmpc/get_rl_cost.m
@@ -1,27 +1,27 @@
-function L = get_rl_cost(n_links, n_origins, n_ramps, TTS, ...
-        max_queue, con_violation_weight)
+function L = get_rl_cost(n_links, n_origins, TTS, ...
+                                                max_queue, con_violation)
     % GET_RL_COST. Returns a casadi.Function for the RL stage cost
     % function.
     arguments
-        n_links , n_origins, n_ramps ...
+        n_links , n_origins ...
             (1, 1) double {mustBePositive,mustBeInteger}
         TTS (1, 1) casadi.Function
-        max_queue (1, :) double
-        con_violation_weight (1, 1) double {mustBePositive}
+        max_queue (:, 1) double
+        con_violation (1, 1) double {mustBePositive}
     end
+    assert(length(max_queue) == n_origins)
 
+    % create symbols
     w = casadi.SX.sym('w', n_origins, 1);
     rho = casadi.SX.sym('rho', n_links, 1);
-
-    if n_ramps == 1
-        L = TTS(w, rho) + ...
-            con_violation_weight * max(0, w(2, :) - max_queue);
-    else
-        L = TTS(w, rho) + ...
-            con_violation_weight * max(0, w(1, :) - max_queue(1)) + ...
-            con_violation_weight * max(0, w(2, :) - max_queue(2));
+    
+    % compute cost
+    L = TTS(w, rho);
+    for i = 1:n_origins
+        if isfinite(max_queue(i))
+            L = L + con_violation * max(0, w(i, :) - max_queue(i));
+        end
     end
     assert(isequal(size(L), [1, 1]));
-
     L = casadi.Function('rl_cost', {w, rho}, {L}, {'w', 'rho'}, {'L'});
 end
