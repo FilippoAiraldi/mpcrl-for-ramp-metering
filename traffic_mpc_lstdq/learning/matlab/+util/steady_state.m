c505b7be4ae1f5a5e1063fa8b8e159829f7bfd3c
Filippo Airaldi <f.airaldi@tudelft.nl>
2022-06-18 12:54:48 +0200

    improved steady-state computations

--- traffic_mpc_lstdq/learning/matlab/+util/steady_state.m
+++ traffic_mpc_lstdq/learning/matlab/+util/steady_state.m
@@ -8,13 +8,14 @@ function [w_ss, rho_ss, v_ss, err, k] = steady_state( ...
         F (1, 1) casadi.Function
         w0, rho0, v0, r, d (:, 1) double
         rho_crit, a, v_free (1, 1) double {mustBeNonnegative}
-        tol (1, 1) double {mustBeNonnegative} = 1e-8
-        maxiter (1, 1) double {mustBeNonnegative,mustBeInteger} = 1e3
+        tol (1, 1) double {mustBeNonnegative} = 1e-3
+        maxiter (1, 1) double {mustBeNonnegative,mustBeInteger} = 5e2
     end
 
     k = 0;
+    err_prev = inf;
     err = inf;
-    while k < maxiter && err >= tol
+    while k < maxiter
         % step the system
         [~, w_ss, ~, rho_ss, v_ss] = F(w0, rho0, v0, r, d, ...
                                                     rho_crit, a, v_free);
@@ -24,9 +25,20 @@ function [w_ss, rho_ss, v_ss, err, k] = steady_state( ...
 
         % compute convergence error
         err = norm(w_ss - w0) + norm(rho_ss - rho0) + norm(v_ss - v0);
+        if err < tol
+            return
+        end
+        if err >= err_prev
+            warning('stopping steady-state search; non-decreasing error')
+            return 
+        end
+
+        % swap and increment
         w0 = w_ss;
         rho0 = rho_ss;
         v0 = v_ss;
         k = k + 1;
+        err_prev = err;
     end
+    warning('steady-state not reached; stopped at max iterations')
 end
