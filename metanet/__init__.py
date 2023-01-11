257e218b5fb06fa8c8f724ce6fdfb4a9a85e1d8f
Filippo Airaldi <filippoairaldi@gmail.com>
2023-01-11 18:38:27 +0100

    merged network.py and steady_state.py

--- metanet/__init__.py
+++ metanet/__init__.py
@@ -2,11 +2,10 @@ __all__ = [
     "create_demands",
     "Demands",
     "get_network",
-    "get_steady_state",
+    "steady_state",
     "HighwayTrafficEnv",
 ]
 
 from metanet.demands import Demands, create_demands
 from metanet.highway_traffic_env import HighwayTrafficEnv
-from metanet.network import get_network
-from metanet.steady_state import get_steady_state
+from metanet.network import get_network, steady_state
--- metanet/network.py
+++ metanet/network.py
@@ -1,7 +1,11 @@
-from typing import Literal, Union
+from typing import Callable, Literal, TypeVar, Union
+from warnings import warn
 
 import casadi as cs
+import numpy as np
 import sym_metanet as metanet
+
+Tx = TypeVar("Tx")
 
 
 def get_network(
@@ -56,3 +60,53 @@ def get_network(
     )
     net.is_valid(raises=True)
     return net, {"rho_crit": rho_crit_sym, "a": a_sym, "v_free": v_free_sym}
+
+
+def steady_state(
+    f: Callable[[Tx], Tx],
+    x0: Tx,
+    tol: float = 1e-3,
+    maxiter: int = 500,
+) -> tuple[Tx, float, int]:
+    r"""Searches the steady-state of the given dynamics.
+
+    Parameters
+    ----------
+    f : Callable[[Tx], Tx]
+        The dynamics function of the form `x+ = f(x)`, where `x` is the state and `x+`
+        the state at the next time instant.
+    x0 : Tx
+        The initial steady-state guess. The type must be compatible with numpy.
+    tol : float, optional
+        Error tolerance for convergence, by default 1e-3.
+    maxiter : int, optional
+        Maximum iterations, by default 500.
+
+    Returns
+    -------
+    Tx
+        The steady-state.
+    error
+        The residual error (less than tolerance, unless error is increasing or maximum
+        iteration treshold has been reached)
+    iters
+        The actual number of iterations performed by the algorithm.
+    """
+    err_previous = float("inf")
+    for k in range(maxiter):
+        x0_ss = f(x0)
+        err = float(np.linalg.norm(x0_ss - x0))  # type: ignore[operator]
+        if err < tol:
+            return x0_ss, err, k
+        elif err > err_previous:
+            warn(
+                "Increasing error encountered in steady-state search "
+                f"({err} > {err_previous})."
+            )
+            return x0_ss, err, k
+
+        x0 = x0_ss
+        err_previous = err
+
+    warn(f"Maximum number of iterations reached in steady-state search ({maxiter}).")
+    return x0_ss, err, k
