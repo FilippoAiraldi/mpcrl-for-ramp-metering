c18122e72ae52a9f4939b1e4e0713fdcad1b9aa8
Filippo Airaldi <filippoairaldi@gmail.com>
2023-01-16 16:23:21 +0100

    updated init

--- metanet/__init__.py
+++ metanet/__init__.py
@@ -1,14 +1,15 @@
 __all__ = [
     "create_demands",
-    "Demands",
     "get_constraint_violation",
     "get_network",
     "get_stage_cost",
     "steady_state",
+    "Constants",
+    "Demands",
     "HighwayTrafficEnv",
 ]
 
 from metanet.costs import get_constraint_violation, get_stage_cost
 from metanet.demands import Demands, create_demands
-from metanet.highway_traffic_env import HighwayTrafficEnv
+from metanet.highway_traffic_env import HighwayTrafficEnv, Constants
 from metanet.network import get_network, steady_state
