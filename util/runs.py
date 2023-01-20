017a2694c34cf15421fff6090c55bb35732b15da
Filippo Airaldi <filippoairaldi@gmail.com>
2023-01-20 19:05:30 +0100

    added tqdm contextmanager compatible with joblib

--- util/runs.py
+++ util/runs.py
@@ -1,7 +1,11 @@
+import contextlib
 import re
 import unicodedata
 from datetime import datetime
 from typing import Optional
+
+import joblib
+from tqdm import tqdm
 
 
 def slugify(value: str, allow_unicode: bool = False) -> str:
@@ -53,3 +57,27 @@ def get_runname(candidate: Optional[str] = None) -> str:
     if candidate is None or not candidate:
         return f'R_{datetime.now().strftime("%Y%m%d_%H%M%S")}'
     return slugify(candidate)
+
+
+@contextlib.contextmanager
+def tqdm_joblib(*args, **kwargs):
+    """Context manager to patch joblib to report into tqdm progress bar given as
+    argument (thanks to https://stackoverflow.com/questions/24983493/
+    tracking-progress-of-joblib-parallel-execution/58936697#58936697)."""
+    tqdm_object = tqdm(*args, **kwargs)
+
+    class TqdmBatchCompletionCallback(joblib.parallel.BatchCompletionCallBack):
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+
+        def __call__(self, *args, **kwargs):
+            tqdm_object.update(n=self.batch_size)
+            return super().__call__(*args, **kwargs)
+
+    old_batch_callback = joblib.parallel.BatchCompletionCallBack
+    joblib.parallel.BatchCompletionCallBack = TqdmBatchCompletionCallback
+    try:
+        yield tqdm_object
+    finally:
+        joblib.parallel.BatchCompletionCallBack = old_batch_callback
+        tqdm_object.close()
