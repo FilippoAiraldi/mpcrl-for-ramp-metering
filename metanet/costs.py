b8d9fa16f834c6263df75af0859508587037933c
Filippo Airaldi <filippoairaldi@gmail.com>
2023-01-20 17:14:39 +0100

    small improvements to cost functions

--- metanet/costs.py
+++ metanet/costs.py
@@ -35,25 +35,27 @@ def get_stage_cost(
     ValueError
         Raises if an origin name cannot be found in the given network.
     """
+    symvar = cs.MX  # faster function evaluations with MX
+
     # compute Total-Time-Spent for the current state
-    TTS = 0
+    TTS = symvar.zeros(1, 1)
     rhos, vs = [], []
     for _, _, link in network.links:
-        rho = cs.MX.sym(f"rho_{link.name}", link.N, 1)
-        v = cs.MX.sym(f"v_{link.name}", link.N, 1)
+        rho = symvar.sym(f"rho_{link.name}", link.N, 1)
+        v = symvar.sym(f"v_{link.name}", link.N, 1)
         rhos.append(rho)
         vs.append(v)
         TTS += cs.sum1(rho) * link.lam * link.L
     ws = []
     for origin in network.origins:
-        w = cs.MX.sym(f"w_{origin.name}", 1, 1)
+        w = symvar.sym(f"w_{origin.name}", 1, 1)
         ws.append(w)
         TTS += cs.sum1(w)
-    TTS *= T  # type: ignore[assignment]
+    TTS *= T
 
     # compute control input variability
-    a = cs.MX.sym("a", n_actions, 1)
-    a_prev = cs.MX.sym("a_prev", n_actions, 1)
+    a = symvar.sym("a", n_actions, 1)
+    a_prev = symvar.sym("a_prev", n_actions, 1)
     VAR = cs.sumsqr(a - a_prev)
 
     # compute constraint violations for origins
@@ -66,6 +68,7 @@ def get_stage_cost(
     )
 
     # pack into function L(s,a) (with a third argument for the previous action)
+    assert TTS.shape == (1, 1) and VAR.shape == (1, 1), "Non-scalar costs."
     s = cs.vertcat(*rhos, *vs, *ws)
     return cs.Function(
         "L", [s, a, a_prev], [TTS, VAR, CVI], ["s", "a", "a-"], ["tts", "var", "cvi"]
