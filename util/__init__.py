05e6220e5a0ec559a570387795b7b5fb6bb35865
Filippo Airaldi <filippoairaldi@gmail.com>
2023-01-20 19:05:46 +0100

    added argument parsing

--- util/__init__.py
+++ util/__init__.py
@@ -1,6 +1,11 @@
 __all__ = [
+    "get_runname",
+    "parse_args",
+    "tqdm_joblib",
     "EnvConstants",
     "MpcConstants",
 ]
 
+from util.arguments import parse_args
 from util.constants import EnvConstants, MpcConstants
+from util.runs import get_runname, tqdm_joblib
---
+++ util/arguments.py
@@ -0,0 +1,115 @@
+import argparse
+from secrets import choice
+
+from util.runs import get_runname
+
+
+def parse_args() -> argparse.Namespace:
+    """Parses the arguments needed to run the highway traffic control environment and
+    its (learning) agents.
+
+    Returns
+    -------
+    argparse.Namespace
+        The argument namespace.
+    """
+
+    # construct parser
+    parser = argparse.ArgumentParser(
+        description="Launches simulation for different MPC-based RL agents.",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+
+    group = parser.add_argument_group("Agent type (select only one)")
+    group.add_argument(
+        "--lstdq",
+        action="store_true",
+        help="If passed, trains a LSTD Q-learning agent",
+    )
+    group.add_argument(
+        "--pk",
+        action="store_true",
+        help="If passed, evaluates a non-learning PK agent.",
+    )
+
+    group = parser.add_argument_group("RL algorithm parameters")
+    group.add_argument("--gamma", type=float, default=1.0, help="Discount factor.")
+    # group.add_argument(  # [3e-2, 3e-2, 1e-3, 1e-3, 1e-3],
+    #     "--lr",
+    #     type=float,
+    #     nargs="+",
+    #     default=[0.498],
+    #     help="Learning rate. Can be a single float, or a list of floats. In "
+    #     "the latter case, either one float per parameter name, or one "
+    #     "per parameter element (in case of parameters that are vectors).",
+    # )
+    # group.add_argument(
+    #     "--perturbation-decay",
+    #     "--perturbation_decay",
+    #     type=float,
+    #     default=0.885,
+    #     help="Rate at which the exploration (random perturbations of the MPC "
+    #     "objective ) decays both in term of chance and strength.",
+    # )
+    # group.add_argument(
+    #     "--max-perc-update",
+    #     "--max_perc_update",
+    #     type=float,
+    #     default=float("inf"),
+    #     help="Limits the maximum value that each parameter can be updated by "
+    #     "a percentage of the current value.",
+    # )
+
+    group = parser.add_argument_group("Simulation length")
+    group.add_argument(
+        "--agents", type=int, default=1, help="Number of agent to simulate."
+    )
+    group.add_argument(
+        "--episodes", type=int, default=10, help="Number of training episodes."
+    )
+    group.add_argument(
+        "--scenarios",
+        type=int,
+        default=10,
+        help="Number of demands' scenarios per training episode.",
+    )
+
+    group = parser.add_argument_group("Simulation details")
+    group.add_argument(
+        "--sym-type", "--sym_type", type=str, choices=("SX", "MX"), default="MX"
+    )
+    group.add_argument(
+        "--runname", type=str, default=None, help="Name of the simulation run."
+    )
+    group.add_argument("--seed", type=int, default=1909, help="RNG seed.")
+    group.add_argument(
+        "--n-jobs", "--n_jobs", type=int, default=-1, help="Simulation parallel jobs."
+    )
+    # group.add_argument("--verbose", action="store_true", help="Verbose flag.")
+
+    # group = parser.add_argument_group("RL experience replay parameters")
+    # group.add_argument(
+    #     "--replay-mem-size",
+    #     "--replay_mem_size",
+    #     type=int,
+    #     default=1,
+    #     help="How many epochs the replay memory can store.",
+    # )
+    # group.add_argument(
+    #     "--replay-mem-sample",
+    #     "--replay_mem_sample",
+    #     type=float,
+    #     default=1.0,
+    #     help="Size of the replay memory samples (percentage).",
+    # )
+
+    args = parser.parse_args()
+
+    # perform some checks and processing
+    assert (
+        args.lstdq + args.pk == 1
+    ), "Must specify one and only one type of agent to simulate."
+    args.runname = get_runname(candidate=args.runname)
+    if args.agents == 1:
+        args.n_jobs = 1  # don't parallelize
+    return args
