c1515405075ba0839d260e1ddfaa9b1e31b712d7
Filippo Airaldi <filippoairaldi@gmail.com>
2023-02-27 17:05:38 +0100

    simplified init file

--- metanet/__init__.py
+++ metanet/__init__.py
@@ -1,13 +1,3 @@
-__all__ = [
-    "create_demands",
-    "get_network",
-    "get_stage_cost",
-    "steady_state",
-    "Demands",
-    "HighwayTrafficEnv",
-]
+__all__ = ["HighwayTrafficEnv"]
 
-from metanet.costs import get_stage_cost
-from metanet.demands import Demands, create_demands
 from metanet.highway_traffic_env import HighwayTrafficEnv
-from metanet.network import get_network, steady_state
