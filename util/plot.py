719da02252885dcea06668b24ff0d3ff2e3b6dfe
Filippo Airaldi <f.airaldi@tudelft.nl>
2023-04-14 13:42:42 +0200

    refactoring of visualization

--- util/plot.py
+++ util/plot.py
@@ -1,5 +1,5 @@
-from itertools import chain, repeat
-from typing import Iterable, Literal, Optional
+from itertools import chain, cycle, repeat
+from typing import Iterable, Literal, Optional, Sequence
 
 import matplotlib as mpl
 import matplotlib.gridspec as gridspec
@@ -8,17 +8,15 @@ import numpy as np
 import numpy.typing as npt
 from matplotlib.axes import Axes
 from matplotlib.figure import Figure
+from matplotlib.lines import Line2D
 from matplotlib.ticker import MaxNLocator
 
-from util.constants import EnvConstants as EC
 from util.constants import STEPS_PER_SCENARIO as K
+from util.constants import EnvConstants as EC
 
-MARKERS = ("o", "s", "v")
+MARKERS = ("o", "s", "v", "^", ">", "<", "P", "x", "D")
 LINESTYLES = ("-", "--", "-.")
-OPTS = {
-    "fill_between.alpha": 0.25,
-    "plot.lw": 2,
-}
+OPTS = {"fill_between.alpha": 0.25}
 
 
 def set_mpl_defaults() -> None:
@@ -26,8 +24,8 @@ def set_mpl_defaults() -> None:
     np.set_printoptions(precision=4)
     plt.style.use("bmh")  # 'seaborn-darkgrid'
     mpl.rcParams["lines.solid_capstyle"] = "round"
-    mpl.rcParams["lines.linewidth"] = 2
-    mpl.rcParams["lines.markersize"] = 3
+    mpl.rcParams["lines.linewidth"] = 1.25
+    mpl.rcParams["lines.markersize"] = 2
     mpl.rcParams["savefig.dpi"] = 600
 
 
@@ -38,8 +36,6 @@ def _set_axis_opts(
     bottom=0,
     top=None,
     intx: bool = False,
-    legend: bool = True,
-    legendloc: str = "best",
 ) -> None:
     """Internal utility to customize the x- and y-axis."""
     for ax in axs:
@@ -47,19 +43,20 @@ def _set_axis_opts(
         ax.set_ylim(bottom=bottom, top=top)
         if intx and not getattr(ax.xaxis.get_major_locator(), "_integer", False):
             ax.xaxis.set_major_locator(MaxNLocator(integer=True))
-        if legend:
-            ax.legend(loc=legendloc)
 
 
-def adjust_limits(figs: Iterable[Figure]) -> None:
-    """Adjusts the axes limits in the figures (ensures all plotted data is shown)."""
-    for fig in figs:
-        axs: list[Axes] = fig.get_axes()
-        for ax in axs:
-            min_y, max_y = ax.dataLim.ymin, ax.dataLim.ymax
-            bottom_y, top_y = ax.get_ylim()
-            if min_y < bottom_y or max_y > top_y:
-                ax.set_ylim(bottom=min_y, top=max_y)
+def _adjust_limits(axs: Iterable[Axes]) -> None:
+    """Adjusts the axes limits (ensures all plotted data is shown)."""
+    for ax in axs:
+        min_y, max_y = ax.dataLim.ymin, ax.dataLim.ymax
+        bottom_y, top_y = ax.get_ylim()
+        if min_y < bottom_y or max_y > top_y:
+            ax.set_ylim(bottom=min_y, top=max_y)
+
+
+def _add_title(fig: Figure, labels: Sequence[str]) -> None:
+    """Adds a title to the figure."""
+    fig.suptitle(" vs ".join(f"{lb} ({ls})" for lb, ls in zip(labels, LINESTYLES)))
 
 
 def _plot_population(
@@ -77,9 +74,7 @@ def _plot_population(
     y_avg = (np.nanmedian if use_median else np.nanmean)(y, 0)  # type: ignore[operator]
     y_std = np.nanstd(y, 0)
     if method == "fill_between":
-        o = ax.plot(
-            x, y_avg, label=label, marker=marker, ls=ls, lw=OPTS["plot.lw"], color=color
-        )[0]
+        o = ax.plot(x, y_avg, label=label, marker=marker, ls=ls, color=color)[0]
         ax.fill_between(
             x,
             y_avg - y_std,
@@ -96,7 +91,6 @@ def _plot_population(
             label=label,
             marker=marker,
             ls=ls,
-            lw=OPTS["plot.lw"],
             errorevery=x.size // 10,
             capsize=5,
             color=color,
@@ -106,41 +100,34 @@ def _plot_population(
 
 
 def plot_traffic_quantities(
-    envsdata: dict[str, npt.NDArray[np.floating]],
-    fig: Figure = None,
-    label: Optional[str] = None,
+    envsdata: list[dict[str, npt.NDArray[np.floating]]],
+    labels: list[str],
     reduce: int = 1,
-    **_,
 ) -> Figure:
-    if fig is None:
-        fig, axs = plt.subplots(3, 2, constrained_layout=True, sharex=True)
-        axs = axs.flatten()
-    else:
-        axs = fig.axes
+    fig, axs_ = plt.subplots(3, 2, constrained_layout=True, sharex=True)
+    axs: Sequence[Axes] = axs_.flatten()
 
     # reduce number of points to plot
     if reduce > 1:
         raxis = 2  # reduction axis
-        envsdata_ = {
-            k: v.take(range(0, v.shape[raxis], reduce), raxis)
-            for k, v in envsdata.items()
-        }
-    else:
-        envsdata_ = envsdata
+        envsdata = [
+            {
+                k: v.take(range(0, v.shape[raxis], reduce), raxis)
+                for k, v in envsdatum.items()
+            }
+            for envsdatum in envsdata
+        ]
 
     # flatten episodes along time
-    envsdata_ = {
-        k: v.reshape(v.shape[0], v.shape[1] * v.shape[2], -1)
-        for k, v in envsdata_.items()
-    }
+    envsdata = [
+        {
+            k: v.reshape(v.shape[0], v.shape[1] * v.shape[2], -1)
+            for k, v in envsdatum.items()
+        }
+        for envsdatum in envsdata
+    ]
 
     # make plots
-    data = (
-        *np.array_split(envsdata_["state"], 3, axis=-1),  # rho, v and w
-        envsdata_["flow"][:, :, -2:],  # origin flow
-        envsdata_["action"],
-        envsdata_["demands"],
-    )
     ylbls = (
         r"$\rho$ (veh/km/lane)",
         r"$v$ (km/h)",
@@ -149,86 +136,138 @@ def plot_traffic_quantities(
         r"$a$ (veh/h)",
         r"$d$ (veh/h, veh/km/lane)",
     )
-    for ax, datum, ylbl in zip(axs, data, ylbls):
-        time = np.arange(datum.shape[1]) * EC.T * EC.steps * reduce
-        datum = np.rollaxis(datum, 2)
-        N = datum.shape[0]
-        if N == 1:
-            _plot_population(ax, time, datum[0], label=label)
-        else:
-            for datum_, ls in zip(datum, LINESTYLES):
-                _plot_population(ax, time, datum_, ls=ls, label=label)
-        ax.set_ylabel(ylbl)
+    for envsdatum, ls in zip(envsdata, LINESTYLES):
+        plotdata = (
+            *np.array_split(envsdatum["state"], 3, axis=-1),  # rho, v and w
+            envsdatum["flow"][:, :, -2:],  # origin flow
+            envsdatum["action"],
+            envsdatum["demands"],
+        )
+        for ax, datum, ylbl in zip(axs, plotdata, ylbls):
+            time = np.arange(datum.shape[1]) * EC.T * EC.steps * reduce
+            datum = np.rollaxis(datum, 2)
+            N = datum.shape[0]
+            if N == 1:
+                _plot_population(ax, time, datum[0], ls=ls)
+            else:
+                for d_ in datum:
+                    _plot_population(ax, time, d_, ls=ls)
+            ax.set_ylabel(ylbl)
+
+    # adjust some opts
     _set_axis_opts(axs)
     for i in (4, 5):
         axs[i].set_xlabel("time (h)")
+    _adjust_limits(axs)
+    _add_title(fig, labels)
     return fig
     # mean_demand = envsdata_["demands"].reshape(10, -1, 120, 3).reshape(-1, 120, 3)
 
 
 def plot_costs(
-    envsdata: dict[str, npt.NDArray[np.floating]],
-    fig: Figure = None,
-    label: Optional[str] = None,
-    **_,
+    envsdata: list[dict[str, npt.NDArray[np.floating]]], labels: list[str]
 ) -> Figure:
-    if fig is None:
-        fig = plt.figure(constrained_layout=True)
-        G = gridspec.GridSpec(2, 2, figure=fig)
-        axs = [fig.add_subplot(G[i]) for i in zip(*np.unravel_index(range(4), (2, 2)))]
-    else:
-        axs = fig.axes
+    fig = plt.figure(constrained_layout=True)
+    G = gridspec.GridSpec(2, 2, figure=fig)
+    axs: list[Axes] = [
+        fig.add_subplot(G[i]) for i in zip(*np.unravel_index(range(4), (2, 2)))
+    ]
 
-    # group as: costs ∈ [n_agents, n_episodes, timesteps, type_of_costs]
+    # process costs
     costnames = ("tts", "var", "cvi", "total")
-    costs = np.stack([envsdata[n] for n in costnames[:-1]], axis=-1)
+    envscosts: list[np.ndarray] = []
+    for envsdatum in envsdata:
+        # group as: costs ∈ [n_agents, n_episodes, timesteps, cost_types]
+        costs = np.stack([envsdatum[n] for n in costnames[:-1]], axis=-1)
 
-    # sum over time - either by episode or by scenario
-    # costs = costs.sum(2)  # sum costs per episodes
-    tax = 2  # time axis
-    n_scenarios = np.ceil(costs.shape[tax] / K).astype(int)
-    costs = np.concatenate(
-        [sc.sum(tax, keepdims=True) for sc in np.array_split(costs, n_scenarios, tax)],
-        axis=tax,
-    ).reshape(-1, n_scenarios, 3)
+        # sum over time - either by episode or by scenario
+        # thiscosts = thiscosts.sum(2)  # sum costs per episodes
+        tax = 2  # time axis
+        n_scenarios = np.ceil(costs.shape[tax] / K).astype(int)
+        splitted_costs = np.array_split(costs, n_scenarios, tax)
+        costs = np.concatenate(
+            [c.sum(tax, keepdims=True) for c in splitted_costs], axis=tax
+        ).reshape(costs.shape[0], -1, costs.shape[-1])
 
-    J = costs.sum(-1, keepdims=True)  # total cost per episode per agent
-    all_costs = np.concatenate((costs, J), axis=-1)
-    ep = np.arange(1, all_costs.shape[1] + 1)
-    for costname, cost, ax in zip(costnames, np.rollaxis(all_costs, 2), axs):
-        _plot_population(ax, ep, cost, label=label)
-        ax.set_ylabel(costname)
+        # add total cost
+        J = costs.sum(-1, keepdims=True)  # total cost per episode per agent
+        costs = np.concatenate((costs, J), axis=-1)
+
+        # append to list
+        envscosts.append(costs)
+
+    # make plotting
+    for costs, ls in zip(envscosts, LINESTYLES):
+        ep = np.arange(1, costs.shape[1] + 1)
+        for costname, cost, ax in zip(costnames, np.rollaxis(costs, 2), axs):
+            _plot_population(ax, ep, cost, ls=ls)
+            ax.set_ylabel(costname)
 
     # set axis options
     _set_axis_opts(axs, intx=True)
     for ax in axs[-2:]:
         ax.set_xlabel("episode")
+    _adjust_limits(axs)
+    _add_title(fig, labels)
     return fig
 
 
 def plot_agent_quantities(
-    agentsdata: dict[str, npt.NDArray[np.floating]],
-    fig: Figure = None,
-    label: Optional[str] = None,
+    agentsdata: list[dict[str, npt.NDArray[np.floating]]],
+    labels: list[str],
     reduce: int = 1,
-    **_,
 ) -> Figure:
     # sourcery skip: low-code-quality
 
-    def plot_parameters(key: str, ax: Axes, label: Optional[str] = None) -> None:
-        n_agents, n_updates = agentsdata[key].shape[:2]
-        updates = np.arange(n_updates)
-        weight = np.rollaxis(agentsdata[key].reshape(n_agents, n_updates, -1), 2)
-        N = weight.shape[0]
-        if N == 1:
-            _plot_population(ax, updates, weight[0], label=label)
-        else:
-            for w, ls in zip(weight, LINESTYLES):
-                _plot_population(ax, updates, w, ls=ls, label=label)
+    def plot_pars(
+        datum: dict[str, npt.NDArray[np.floating]],
+        keys: list[str],
+        ax: Axes,
+        ls: str,
+        legend: bool = False,
+    ) -> None:
+        # prepare some quantities
+        markers = cycle(MARKERS)
+        labels_and_markers = []
 
+        # loop over keys
+        for key, marker in zip(keys, markers):
+            if key not in datum:
+                continue
+
+            # rearrange data
+            weight = datum[key]
+            n_agents, n_updates, size1, size2 = weight.shape
+            N = size1 * size2
+            updates = np.arange(n_updates)
+            w = np.rollaxis(weight.reshape(n_agents, n_updates, N), 2)
+
+            # plot
+            if N == 1:
+                if len(keys) == 1:
+                    marker = None
+                else:
+                    marker = next(markers)
+                    labels_and_markers.append((key, marker))
+                _plot_population(ax, updates, w[0], marker=marker, ls=ls)
+            else:
+                for i, (w_, marker) in enumerate(zip(w, markers)):
+                    _plot_population(ax, updates, w_, marker=marker, ls=ls)
+                    labels_and_markers.append((f"{key}_{i}", marker))
+
+        # add legend
+        if legend and labels_and_markers:
+            ax.legend(
+                handles=[
+                    Line2D([], [], color="k", marker=marker, label=lbl)
+                    for lbl, marker in labels_and_markers
+                ]
+            )
+
+    # decide what to plot
     a_key, td_errors_key = None, None
     v_free_keys, rho_crit_keys, weight_keys = [], [], []
-    for key in agentsdata:
+    for key in set(chain.from_iterable(agentsdata)):
         if key == "a":
             a_key = key
         elif key == "td_errors":
@@ -251,60 +290,52 @@ def plot_agent_quantities(
     assert nplots > 0, "No agent's quantities to plot."
     ncols = int(np.round(np.sqrt(nplots)))
     nrows = int(np.ceil(nplots / ncols))
-    if fig is None:
-        fig = plt.figure(constrained_layout=True)
-        G = gridspec.GridSpec(nrows, ncols, figure=fig)
-        axs = [
-            fig.add_subplot(G[idx])
-            for idx in zip(*np.unravel_index(range(nplots), (nrows, ncols)))
-        ]
-    else:
-        axs = fig.axes
-    axs_iter = iter(axs)
+    fig = plt.figure(constrained_layout=True)
+    G = gridspec.GridSpec(nrows, ncols, figure=fig)
+    axs: list[Axes] = [
+        fig.add_subplot(G[idx])
+        for idx in zip(*np.unravel_index(range(nplots), (nrows, ncols)))
+    ]
 
-    # plot a
-    if a_key is not None:
-        ax = next(axs_iter)
-        plot_parameters(a_key, ax, f"a ({label})")
-        ax.set_ylabel(r"$a$")
+    # make plots
+    a_ax, rho_ax, v_ax = None, None, None
+    for agentsdatum, ls in zip(agentsdata, LINESTYLES):
+        axs_iter = iter(axs)
 
-    # plot rho_crit
-    if rho_crit_keys:
-        ax = next(axs_iter)
-        for rho_crit_key in rho_crit_keys:
-            plot_parameters(rho_crit_key, ax, label=f"{rho_crit_key} ({label})")
-        ax.hlines(
-            EC.rho_crit, *ax.get_xlim(), colors="k", ls="--", lw=OPTS["plot.lw"] / 2
-        )
-        ax.set_ylabel(r"$\rho_{crit}$")
+        # plot a
+        if a_key is not None:
+            a_ax = next(axs_iter)
+            plot_pars(agentsdatum, [a_key], a_ax, ls, legend=True)
+            a_ax.set_ylabel(r"$a$")
 
-    # plot v_free
-    if v_free_keys:
-        ax = next(axs_iter)
-        for v_free_key in v_free_keys:
-            plot_parameters(v_free_key, ax, label=f"{v_free_key} ({label})")
-        ax.hlines(
-            EC.v_free, *ax.get_xlim(), colors="k", ls="--", lw=OPTS["plot.lw"] / 2
-        )
-        ax.set_ylabel(r"$v_{free}$")
+        # plot rho_crit
+        if rho_crit_keys:
+            rho_ax = next(axs_iter)
+            plot_pars(agentsdatum, rho_crit_keys, rho_ax, ls, legend=True)
+            rho_ax.set_ylabel(r"$\rho_{crit}$")
 
-    # plot other weights
-    for weight_key in weight_keys:
-        ax = next(axs_iter)
-        plot_parameters(weight_key, ax)
-        ax.set_ylabel(weight_key.replace("_", " "), fontsize=9)
+        # plot v_free
+        if v_free_keys:
+            v_ax = next(axs_iter)
+            plot_pars(agentsdatum, v_free_keys, v_ax, ls, legend=True)
+            v_ax.set_ylabel(r"$v_{free}$")
 
-    # plot td_error
-    if td_errors_key is not None:
-        ax = next(axs_iter)
-        td_errors = agentsdata[td_errors_key][:, ::reduce]
-        time = np.arange(td_errors.shape[1]) * EC.T * EC.steps * reduce
-        _plot_population(ax, time, td_errors, label=label)
-        ax.set_ylabel(r"$\delta$")
+        # plot other weights
+        for weight_key in weight_keys:
+            ax = next(axs_iter)
+            plot_pars(agentsdatum, [weight_key], ax, ls)
+            ax.set_ylabel(weight_key.replace("_", " "), fontsize=9)
 
-    # set all axes' options
-    _set_axis_opts(axs, bottom=None, intx=True, legend=False)
+        # plot td_error
+        if td_errors_key is not None:
+            ax = next(axs_iter)
+            td_errors = agentsdatum[td_errors_key][:, ::reduce]
+            time = np.arange(td_errors.shape[1]) * EC.T * EC.steps * reduce
+            _plot_population(ax, time, td_errors, ls=ls)
+            ax.set_ylabel(r"$\delta$")
 
+    # set axis options
+    _set_axis_opts(axs, bottom=None, intx=True)
     # set x labels in bottom axes
     nxlbls = ncols - (ncols * nrows - nplots)
     xlbls = chain(
@@ -313,4 +344,12 @@ def plot_agent_quantities(
     )
     for ax, xlbl in zip(reversed(axs), xlbls):
         ax.set_xlabel(xlbl)
+    _adjust_limits(axs)
+    _add_title(fig, labels)
+
+    # plot true parameters
+    plot_ = lambda v, ax: ax.hlines(v, *ax.get_xlim(), colors="darkgrey", zorder=-1e3)
+    for val, ax in zip((EC.a, EC.rho_crit, EC.v_free), (a_ax, rho_ax, v_ax)):
+        if ax is not None:
+            plot_(val, ax)
     return fig
--- visualization.py
+++ visualization.py
@@ -1,22 +1,24 @@
 from pathlib import Path
-from typing import Any, Callable, Iterable, Iterator
+from typing import Iterable, Iterator
 
 import matplotlib.pyplot as plt
-from matplotlib.figure import Figure
+import numpy as np
 
 from util import load_data, parse_visualization_args, plot
 
 
-def load_all_data(filenames: Iterable[str]) -> Iterator[tuple[str, dict[str, Any]]]:
+def load_all_data(
+    filenames: Iterable[str],
+) -> Iterator[tuple[str, dict[str, np.ndarray], dict[str, np.ndarray]]]:
     """Loads the data from the filenames and reduces their size."""
     for i, fn in enumerate(filenames):
-        data = load_data(fn)
         name = Path(fn).stem
-        details = (
-            f" -{k}: {v}" for k, v in data.items() if k not in ("envs", "agents")
-        )
+        data = load_data(fn)
+        envsdata = data.pop("envs")
+        agentsdata = data.pop("agents", {})
+        details = (f" -{k}: {v}" for k, v in data.items())
         print(f"p{i}) {name}", *details, sep="\n")
-        yield name, data
+        yield name, envsdata, agentsdata
 
 
 if __name__ == "__main__":
@@ -24,27 +26,14 @@ if __name__ == "__main__":
     args = parse_visualization_args()
     plot.set_mpl_defaults()
 
-    # load and plot data one by one
-    figs: dict[Callable[[Any], Figure], Figure] = {}
-    for name, data in load_all_data(args.filenames):
-        envsdata = data["envs"]
-        agentsdata = data.get("agents", None)
+    # load all data and print simulation details
+    names, envsdata, agentsdata = zip(*load_all_data(args.filenames))
 
-        funcs: list[Callable[[Any], Figure]] = []
-        if args.traffic:
-            funcs.append(plot.plot_traffic_quantities)
-        if args.cost:
-            funcs.append(plot.plot_costs)
-        if agentsdata is not None and args.agent:
-            funcs.append(plot.plot_agent_quantities)
-
-        for fun in funcs:
-            figs[fun] = fun(  # type: ignore[call-arg]
-                envsdata=envsdata,
-                agentsdata=agentsdata,
-                fig=figs.get(fun, None),
-                label=name,
-                reduce=args.reduce,
-            )
-    plot.adjust_limits(figs.values())
+    # plot each figure at a time
+    if args.traffic:
+        plot.plot_traffic_quantities(envsdata, names, reduce=args.reduce)
+    if args.cost:
+        plot.plot_costs(envsdata, names)
+    if args.agent:
+        plot.plot_agent_quantities(agentsdata, names, reduce=args.reduce)
     plt.show()
