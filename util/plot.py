d29ecb5bbbe4dc4682a4bf78991e1a6b14e429c7
Filippo Airaldi <f.airaldi@tudelft.nl>
2023-09-01 08:57:30 +0200

    improving plots

--- util/plot.py
+++ util/plot.py
@@ -24,7 +24,7 @@ def set_mpl_defaults() -> None:
     np.set_printoptions(precision=4)
     plt.style.use("bmh")  # 'seaborn-darkgrid'
     mpl.rcParams["lines.solid_capstyle"] = "round"
-    mpl.rcParams["lines.linewidth"] = 1.25
+    mpl.rcParams["lines.linewidth"] = 1.5
     mpl.rcParams["lines.markersize"] = 2
     mpl.rcParams["savefig.dpi"] = 600
 
@@ -67,36 +67,20 @@ def _plot_population(
     marker: Optional[str] = None,
     ls: Optional[str] = None,
     label: Optional[str] = None,
-    method: Literal["fill_between", "errorbar"] = "fill_between",
     color: Optional[str] = None,
 ) -> None:
     """Internal utility to plot a quantity from some population of envs/agents."""
     y_avg = (np.nanmedian if use_median else np.nanmean)(y, 0)  # type: ignore[operator]
     y_std = np.nanstd(y, 0)
-    if method == "fill_between":
-        o = ax.plot(x, y_avg, label=label, marker=marker, ls=ls, color=color)[0]
-        ax.fill_between(
-            x,
-            y_avg - y_std,
-            y_avg + y_std,
-            alpha=OPTS["fill_between.alpha"],
-            color=o.get_color(),
-            label=None,
-        )
-    elif method == "errorbar":
-        ax.errorbar(
-            x,
-            y_avg,
-            y_std,
-            label=label,
-            marker=marker,
-            ls=ls,
-            errorevery=x.size // 10,
-            capsize=5,
-            color=color,
-        )
-    else:
-        raise ValueError(f"unsupported plotting method {method}.")
+    handles = ax.plot(x, y_avg, label=label, marker=marker, ls=ls, color=color)
+    ax.fill_between(
+        x,
+        y_avg - y_std,
+        y_avg + y_std,
+        alpha=OPTS["fill_between.alpha"],
+        color=handles[0].get_color(),
+        label=None,
+    )
 
 
 def _save2tikz(*figs: Figure) -> None:
@@ -126,21 +110,11 @@ def plot_traffic_quantities(
         # plot demands
         fig1, axs = plt.subplots(2, 1, constrained_layout=True, sharex=True)
         all_demands = envsdatum["demands"].reshape(-1, K, 3)
-        all_demands_avg = all_demands.mean(0)
-        all_demands_std = all_demands.std(0)
-        time = np.arange(all_demands_avg.shape[0]) * EC.T * EC.steps * 60
+        time = np.arange(all_demands.shape[1]) * EC.T * EC.steps * 60
         for i, (ax, lbl) in enumerate(
             [(axs[0], "$O_1$"), (axs[0], "$O_2$"), (axs[1], "$D_1$")]
         ):
-            ax.fill_between(
-                time,
-                all_demands_avg[:, i] - 2 * all_demands_std[:, i],
-                all_demands_avg[:, i] + 2 * all_demands_std[:, i],
-                alpha=OPTS["fill_between.alpha"],
-                color=f"C{i}",
-                label=None,
-            )
-            ax.plot(time, all_demands_avg[:, i], color=f"C{i}", ls="--", lw=0.75)
+            _plot_population(ax, time, all_demands[..., i], ls="--", color=f"C{i}", )
             idx = np_random.integers(all_demands.shape[0])
             ax.plot(time, all_demands[idx, :, i], color=f"C{i}", label=lbl)
         _adjust_limits(axs)
@@ -151,24 +125,16 @@ def plot_traffic_quantities(
 
         # plot 1st, middle, and last on-ramp queues
         fig2, ax = plt.subplots(1, 1, constrained_layout=True, sharex=True)
-        idx = [0, 3, -1]
-        O2_queue = envsdatum["state"][:, idx, :, -1]
-        O2_queue_avg = O2_queue.mean(1)
-        O2_queue_std = O2_queue.std(1)
-        alpha = OPTS["fill_between.alpha"]
-        time = np.arange(O2_queue_avg.shape[1]) * EC.T * EC.steps * 60
+        idx = [0, 1, 2, 3, 20]
+        O2_queue = envsdatum["state"][..., -1]
+        time = np.arange(O2_queue.shape[2]) * EC.T * EC.steps * 60
         ax.axhline(y=EC.ramp_max_queue["O2"], color="k", ls="--", label=None)
-        lbls = ("First episode", f"Episode {idx[1] + 1}", "Last episode")
-        for i, (avg, std, lbl) in enumerate(zip(O2_queue_avg, O2_queue_std, lbls)):
-            ax.fill_between(
-                time, avg - std, avg + std, alpha=alpha, color=f"C{i}", label=None
-            )
-            ax.plot(time, avg, color=f"C{i}", label=lbl)
+        for i, j in enumerate(idx):
+            _plot_population(ax, time, O2_queue[:, j], label=f"Ep. {j}", color=f"C{i}")
         _adjust_limits((ax,))
-        ax.set_ylim(bottom=0)
         ax.set_ylabel("$O2$ queue (veh)")
         ax.set_xlabel("time (min)")
-        ax.legend(loc="upper left")
+        ax.legend(loc="upper right", ncol=1)
 
         _save2tikz(fig1, fig2)
     else:
@@ -220,190 +186,185 @@ def plot_traffic_quantities(
 
 
 def plot_costs(
-    envsdata: list[dict[str, npt.NDArray[np.floating]]],
-    labels: list[str],
-    paper: bool,
+    envsdata: list[dict[str, npt.NDArray[np.floating]]], labels: list[str], paper: bool
 ) -> None:
-    fig, axs = plt.subplots(1, 3, constrained_layout=True, sharex=True)
+    fig, axs = plt.subplots(3, 1, constrained_layout=True, sharex=True)
 
     # process costs
     costnames = ("tts", "var", "cvi")
+    ylbls = ("TTS", "Control variability", "Constraint violation")
     envscosts: list[np.ndarray] = []
     for envsdatum in envsdata:
-        # group as: costs ∈ [n_agents, n_episodes, timesteps, cost_types]
-        costs = np.stack([envsdatum[n] for n in costnames], axis=-1)
-
-        # sum over time - either by episode or by scenario
-        # costs = costs.sum(2)  # sum costs per episodes
-        tax = 2  # time axis
-        n_scenarios = np.ceil(costs.shape[tax] / K).astype(int)
-        splitted_costs = np.array_split(costs, n_scenarios, tax)
-        costs = np.concatenate(
-            [c.sum(tax, keepdims=True) for c in splitted_costs], axis=tax
-        ).reshape(costs.shape[0], -1, costs.shape[-1])
-
-        # append to list
+        costs = np.stack([envsdatum[n].sum(2) for n in costnames], axis=-1)
         envscosts.append(costs)
 
     # make plotting
     for costs, ls in zip(envscosts, LINESTYLES):
-        ep = np.arange(costs.shape[1]) / n_scenarios
-        for costname, cost, ax in zip(costnames, np.rollaxis(costs, 2), axs):
+        ep = np.arange(costs.shape[1])
+        for ylbl, cost, ax in zip(ylbls, np.rollaxis(costs, 2), axs):
             _plot_population(ax, ep, cost, ls=ls)
-            ax.set_ylabel(costname)
+            ax.set_ylabel(ylbl)
 
     # set axis options
-    _set_axis_opts(axs, intx=True)
-    for ax in axs:
-        ax.set_xlabel("episode")
+    axs[-1].set_xlabel("Learning episode")
     _adjust_limits(axs)
+    _set_axis_opts(axs, intx=True, right=30, bottom=None)
     _add_title(fig, labels)
+    if paper:
+        _save2tikz(fig)
 
 
 def plot_agent_quantities(
-    agentsdata: list[dict[str, npt.NDArray[np.floating]]], labels: list[str], paper: bool,
+    agentsdata: list[dict[str, npt.NDArray[np.floating]]],
+    labels: list[str],
+    paper: bool,
 ) -> None:
     # sourcery skip: low-code-quality
 
-    def plot_pars(
-        datum: dict[str, npt.NDArray[np.floating]],
-        keys: list[str],
-        ax: Axes,
-        ls: str,
-        legend: bool = False,
-    ) -> None:
-        # prepare some quantities
-        markers = cycle(MARKERS)
-        labels_and_markers = []
+    if paper:
+        # plot TD error
+        pass
+    else:
 
-        # loop over keys
-        for key, marker in zip(keys, markers):
-            if key not in datum:
-                continue
+        def plot_pars(
+            datum: dict[str, npt.NDArray[np.floating]],
+            keys: list[str],
+            ax: Axes,
+            ls: str,
+            legend: bool = False,
+        ) -> None:
+            # prepare some quantities
+            markers = cycle(MARKERS)
+            labels_and_markers = []
 
-            # rearrange data
-            weight = datum[key]
-            n_agents, n_updates, size1, size2 = weight.shape
-            N = size1 * size2
-            updates = np.arange(n_updates)
-            w = np.rollaxis(weight.reshape(n_agents, n_updates, N), 2)
+            # loop over keys
+            for key, marker in zip(keys, markers):
+                if key not in datum:
+                    continue
 
-            # plot
-            if N == 1:
-                if len(keys) == 1:
-                    marker = None  # type: ignore[assignment]
+                # rearrange data
+                weight = datum[key]
+                n_agents, n_updates, size1, size2 = weight.shape
+                N = size1 * size2
+                updates = np.arange(n_updates)
+                w = np.rollaxis(weight.reshape(n_agents, n_updates, N), 2)
+
+                # plot
+                if N == 1:
+                    if len(keys) == 1:
+                        marker = None  # type: ignore[assignment]
+                    else:
+                        marker = next(markers)
+                        labels_and_markers.append((key, marker))
+                    _plot_population(ax, updates, w[0], marker=marker, ls=ls)
                 else:
-                    marker = next(markers)
-                    labels_and_markers.append((key, marker))
-                _plot_population(ax, updates, w[0], marker=marker, ls=ls)
+                    for i, (w_, marker) in enumerate(zip(w, markers)):
+                        _plot_population(ax, updates, w_, marker=marker, ls=ls)
+                        labels_and_markers.append((f"{key}_{i}", marker))
+
+            # add legend
+            if legend and labels_and_markers:
+                ax.legend(
+                    handles=[
+                        Line2D([], [], color="k", marker=marker, label=lbl)
+                        for lbl, marker in labels_and_markers
+                    ]
+                )
+
+        # decide what to plot
+        a_key, td_errors_key = None, None
+        v_free_keys, rho_crit_keys, weight_keys = [], [], []
+        for key in set(chain.from_iterable(agentsdata)):
+            if key == "a":
+                a_key = key
+            elif key == "td_errors":
+                td_errors_key = key
+            elif key.startswith("v_free"):
+                v_free_keys.append(key)
+            elif key.startswith("rho_crit"):
+                rho_crit_keys.append(key)
+            elif key.startswith("weight_"):
+                weight_keys.append(key)
             else:
-                for i, (w_, marker) in enumerate(zip(w, markers)):
-                    _plot_population(ax, updates, w_, marker=marker, ls=ls)
-                    labels_and_markers.append((f"{key}_{i}", marker))
+                raise RuntimeError(f"unexpected key '{key}' in agent's data.")
+        nplots = (
+            (a_key is not None)
+            + (td_errors_key is not None)
+            + (len(v_free_keys) > 0)
+            + (len(rho_crit_keys) > 0)
+            + len(weight_keys)
+        )
+        assert nplots > 0, "No agent's quantities to plot."
+        ncols = int(np.round(np.sqrt(nplots)))
+        nrows = int(np.ceil(nplots / ncols))
+        fig = plt.figure(constrained_layout=True)
+        G = gridspec.GridSpec(nrows, ncols, figure=fig)
+        axs: list[Axes] = [
+            fig.add_subplot(G[idx])
+            for idx in zip(*np.unravel_index(range(nplots), (nrows, ncols)))
+        ]
 
-        # add legend
-        if legend and labels_and_markers:
-            ax.legend(
-                handles=[
-                    Line2D([], [], color="k", marker=marker, label=lbl)
-                    for lbl, marker in labels_and_markers
-                ]
-            )
+        # make plots
+        a_ax, rho_ax, v_ax = None, None, None
+        for agentsdatum, ls in zip(agentsdata, LINESTYLES):
+            axs_iter = iter(axs)
 
-    # decide what to plot
-    a_key, td_errors_key = None, None
-    v_free_keys, rho_crit_keys, weight_keys = [], [], []
-    for key in set(chain.from_iterable(agentsdata)):
-        if key == "a":
-            a_key = key
-        elif key == "td_errors":
-            td_errors_key = key
-        elif key.startswith("v_free"):
-            v_free_keys.append(key)
-        elif key.startswith("rho_crit"):
-            rho_crit_keys.append(key)
-        elif key.startswith("weight_"):
-            weight_keys.append(key)
-        else:
-            raise RuntimeError(f"unexpected key '{key}' in agent's data.")
-    nplots = (
-        (a_key is not None)
-        + (td_errors_key is not None)
-        + (len(v_free_keys) > 0)
-        + (len(rho_crit_keys) > 0)
-        + len(weight_keys)
-    )
-    assert nplots > 0, "No agent's quantities to plot."
-    ncols = int(np.round(np.sqrt(nplots)))
-    nrows = int(np.ceil(nplots / ncols))
-    fig = plt.figure(constrained_layout=True)
-    G = gridspec.GridSpec(nrows, ncols, figure=fig)
-    axs: list[Axes] = [
-        fig.add_subplot(G[idx])
-        for idx in zip(*np.unravel_index(range(nplots), (nrows, ncols)))
-    ]
+            # plot a
+            if a_key is not None:
+                a_ax = next(axs_iter)
+                plot_pars(agentsdatum, [a_key], a_ax, ls, legend=True)
+                a_ax.set_ylabel(r"$a$")
 
-    # make plots
-    a_ax, rho_ax, v_ax = None, None, None
-    for agentsdatum, ls in zip(agentsdata, LINESTYLES):
-        axs_iter = iter(axs)
+            # plot rho_crit
+            if rho_crit_keys:
+                rho_ax = next(axs_iter)
+                plot_pars(agentsdatum, rho_crit_keys, rho_ax, ls, legend=True)
+                rho_ax.set_ylabel(r"$\rho_{crit}$")
 
-        # plot a
-        if a_key is not None:
-            a_ax = next(axs_iter)
-            plot_pars(agentsdatum, [a_key], a_ax, ls, legend=True)
-            a_ax.set_ylabel(r"$a$")
+            # plot v_free
+            if v_free_keys:
+                v_ax = next(axs_iter)
+                plot_pars(agentsdatum, v_free_keys, v_ax, ls, legend=True)
+                v_ax.set_ylabel(r"$v_{free}$")
 
-        # plot rho_crit
-        if rho_crit_keys:
-            rho_ax = next(axs_iter)
-            plot_pars(agentsdatum, rho_crit_keys, rho_ax, ls, legend=True)
-            rho_ax.set_ylabel(r"$\rho_{crit}$")
+            # plot other weights
+            for weight_key in weight_keys:
+                ax = next(axs_iter)
+                plot_pars(agentsdatum, [weight_key], ax, ls)
+                ax.set_ylabel(weight_key.replace("_", " "), fontsize=9)
 
-        # plot v_free
-        if v_free_keys:
-            v_ax = next(axs_iter)
-            plot_pars(agentsdatum, v_free_keys, v_ax, ls, legend=True)
-            v_ax.set_ylabel(r"$v_{free}$")
+            # plot td_error
+            if td_errors_key is not None:
+                ax = next(axs_iter)
+                td_data = agentsdatum[td_errors_key]
+                n_agents, n_timesteps = td_data.shape
+                n_scenarios_per_episode = next(
+                    i for i in range(1, 11) if n_timesteps % (K * i - 1) == 0
+                )
+                td_errors = np.nanmean(
+                    td_data.reshape(n_agents, -1, n_scenarios_per_episode * K - 1), -1
+                )
+                episodes = np.arange(td_errors.shape[1])
+                _plot_population(ax, episodes, td_errors, ls=ls)
+                ax.set_ylabel(r"$\delta$")
 
-        # plot other weights
-        for weight_key in weight_keys:
-            ax = next(axs_iter)
-            plot_pars(agentsdatum, [weight_key], ax, ls)
-            ax.set_ylabel(weight_key.replace("_", " "), fontsize=9)
+        # set axis options
+        _set_axis_opts(axs, bottom=None, intx=True)
+        # set x labels in bottom axes
+        nxlbls = ncols - (ncols * nrows - nplots)
+        xlbls = chain(
+            (("update" if td_errors_key is None else "episode"),),
+            repeat("update", nxlbls - 1),
+        )
+        for ax, xlbl in zip(reversed(axs), xlbls):
+            ax.set_xlabel(xlbl)
+        _adjust_limits(axs)
+        _add_title(fig, labels)
 
-        # plot td_error
-        if td_errors_key is not None:
-            ax = next(axs_iter)
-            # TODO: decide which TD error plot is better
-            # td_errors = _moving_average(agentsdatum[td_errors_key], K)
-            td_data = agentsdatum[td_errors_key]
-            n_agents, n_timesteps = td_data.shape
-            n_scenarios_per_episode = next(
-                i for i in range(1, 11) if n_timesteps % (K * i - 1) == 0
-            )
-            td_errors = np.nanmean(
-                td_data.reshape(n_agents, -1, n_scenarios_per_episode * K - 1), -1
-            )
-            episodes = np.arange(td_errors.shape[1])
-            _plot_population(ax, episodes, td_errors, ls=ls)
-            ax.set_ylabel(r"$\delta$")
-
-    # set axis options
-    _set_axis_opts(axs, bottom=None, intx=True)
-    # set x labels in bottom axes
-    nxlbls = ncols - (ncols * nrows - nplots)
-    xlbls = chain(
-        (("update" if td_errors_key is None else "episode"),),
-        repeat("update", nxlbls - 1),
-    )
-    for ax, xlbl in zip(reversed(axs), xlbls):
-        ax.set_xlabel(xlbl)
-    _adjust_limits(axs)
-    _add_title(fig, labels)
-
-    # plot true parameters
-    plot_ = lambda v, ax: ax.hlines(v, *ax.get_xlim(), colors="darkgrey", zorder=-1e3)
-    for val, ax in zip((EC.a, EC.rho_crit, EC.v_free), (a_ax, rho_ax, v_ax)):
-        if ax is not None:
-            plot_(val, ax)
+        # plot true parameters
+        plot_ = lambda v, ax: ax.hlines(
+            v, *ax.get_xlim(), colors="darkgrey", zorder=-1e3
+        )
+        for val, ax in zip((EC.a, EC.rho_crit, EC.v_free), (a_ax, rho_ax, v_ax)):
+            if ax is not None:
+                plot_(val, ax)
